3ff818a7c0a00e6e673058c97679283f44fbf170
Carrie Mathieu <carrieburks@gmail.com>
2021-10-20 10:32:35 -0400

    add card form component

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css']
+})
+export class CardFormComponent implements OnInit {
+
+  cardForm = new FormGroup({
+    // validators.required - value must be present
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ])
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
